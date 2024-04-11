655c988ae0ab85953682247fd8d2384b966bfee9
gawaines <zhang.leh@northeastern.edu>
2024-04-11 00:42:45 -0700

    Minor Fix

--- pages/2📊Application_Data_Insight.py
+++ pages/2📊Application_Data_Insight.py
@@ -11,6 +11,10 @@ features_1 = ["Employer", "Citizenship", "Class of Admission", "Agent Firm" , "J
 columns_1 = ["employer", "citizenship", "class_of_admission", "agent_firm" , "job_title"]
 features_2 = ["Education Level", "Experience Level", "Wage Level"]
 columns_2 = ["education_level", "experience_level" , "wage_level" ]
+wage_level_list = ['Level 1: Below 50K', 'Level 2: 50K-75K', 'Level 3: 75K-100K', 'Level 4: 100K-150K','Level 5: Above 150K']
+education_list = ['High School', "Associate's", "Bachelor's", "Master's", 'Doctorate', 'Other']
+experience_list = ['Level 1: 0-12 months', 'Level 2: 13-36 months', 'Level 3: 37-60 months', 'Level 4: 61-120 months', 'Level 5: 121+ months']
+option_lists = [education_list, experience_list, wage_level_list]
 certified_cases = df[df['case_status'] == 'Certified']
 denied_cases = df[df['case_status'] == 'Denied']
 
@@ -50,9 +54,14 @@ def load_options(file):
     with open('./data/' + file + '.txt', 'r') as file:
         options = file.readlines()
         options = [option.strip() for option in options]
-    return options
+    return options    
 
-def write_result(column, selection):
+selection = st.sidebar.radio("Select a Feature for Visualization", features_1 + features_2)
+if selection in features_1:
+    column = columns_1[features_1.index(selection)]
+    draw_top("Top 10 " + selection + " by Number of Certified Cases", column, certified_cases, "Number of Certified Applications", "orange")
+    draw_top("Top 10 " + selection + " by Number of Denied Cases", column, denied_cases, "Number of Denied Applications", '#1f77b4')
+    draw_rate("Bottom 10 " + selection + " by Cerfication Rate", column)
     st.markdown("### Your Choice of " + selection)
     options = load_options(column)
     choice = st.selectbox("**Select your " + selection + ".**", options)
@@ -61,18 +70,16 @@ def write_result(column, selection):
     st.markdown("### Number of Certified Cases: " + str(certified))
     st.markdown("### Number of Denied Cases: " + str(denied))
     st.markdown("### Certificate Rate: " + str(100.0 * certified / (denied + certified)) + "%")
-
-selection = st.sidebar.radio("Select a Feature for Visualization", features_1 + features_2)
-
-if selection in features_1:
-    column = columns_1[features_1.index(selection)]
-    draw_top("Top 10 " + selection + " by Number of Certified Cases", column, certified_cases, "Number of Certified Applications", "orange")
-    draw_top("Top 10 " + selection + " by Number of Denied Cases", column, denied_cases, "Number of Denied Applications", '#1f77b4')
-    draw_rate("Bottom 10 " + selection + " by Cerfication Rate", column)
-    write_result(column, selection)
 else:
     column = columns_2[features_2.index(selection)]
     draw_distribution("Percentage of Certified Applications by " + selection, column)
     draw_rate("Certified Rate Rank", column)
-    write_result(column)
+    st.markdown("### Your Choice of " + selection)
+    options = option_lists[features_2.index(selection)]
+    choice = st.selectbox("**Select your " + selection + ".**", options)
+    certified = len(certified_cases[certified_cases[column] == choice])
+    denied = len(denied_cases[denied_cases[column] == choice])
+    st.markdown("### Number of Certified Cases: " + str(certified))
+    st.markdown("### Number of Denied Cases: " + str(denied))
+    st.markdown("### Certificate Rate: " + str(100.0 * certified / (denied + certified)) + "%")
 
