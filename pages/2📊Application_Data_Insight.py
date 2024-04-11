81e6f1b8bc89e15d17553ee5ee12367e1abd89e0
gawaines <zhang.leh@northeastern.edu>
2024-04-11 00:37:12 -0700

    feature selection

--- pages/2📊Application_Data_Insight.py
+++ pages/2📊Application_Data_Insight.py
@@ -7,6 +7,13 @@ import joblib
 st.title('PERM Application Data Visualization')
 df = joblib.load('./data/dataframe.joblib')
 
+features_1 = ["Employer", "Citizenship", "Class of Admission", "Agent Firm" , "Job Title"]
+columns_1 = ["employer", "citizenship", "class_of_admission", "agent_firm" , "job_title"]
+features_2 = ["Education Level", "Experience Level", "Wage Level"]
+columns_2 = ["education_level", "experience_level" , "wage_level" ]
+certified_cases = df[df['case_status'] == 'Certified']
+denied_cases = df[df['case_status'] == 'Denied']
+
 def draw_graph_title(title):
     st.markdown("### " + title)
     st.pyplot(plt)
@@ -39,27 +46,30 @@ def draw_distribution(title, column):
     plt.ylabel('')
     draw_graph_title(title)
 
-def write_result(column):
-    st.write(column)
-    st.write("Number of Certified Cases: ", 0)
-    st.write("Number of Denied Cases: ", 0)
-    st.write("Certificate Rate: ", 0)
+def load_options(file):
+    with open('./data/' + file + '.txt', 'r') as file:
+        options = file.readlines()
+        options = [option.strip() for option in options]
+    return options
 
-features_1 = ["Employer", "Citizenship", "Class of Admission", "Agent Firm" , "Job Title"]
-columns_1 = ["employer", "citizenship", "class_of_admission", "agent_firm" , "job_title"]
-features_2 = ["Education Level", "Experience Level", "Wage Level"]
-columns_2 = ["education_level", "experience_level" , "wage_level" ]
+def write_result(column, selection):
+    st.markdown("### Your Choice of " + selection)
+    options = load_options(column)
+    choice = st.selectbox("**Select your " + selection + ".**", options)
+    certified = len(certified_cases[certified_cases[column] == choice])
+    denied = len(denied_cases[denied_cases[column] == choice])
+    st.markdown("### Number of Certified Cases: ", certified )
+    st.markdown("### Number of Denied Cases: ", denied)
+    st.markdown("### Certificate Rate: ", certified / (denied + certified))
+
 selection = st.sidebar.radio("Select a Feature for Visualization", features_1 + features_2)
-
-certified_cases = df[df['case_status'] == 'Certified']
-denied_cases = df[df['case_status'] == 'Denied']
 
 if selection in features_1:
     column = columns_1[features_1.index(selection)]
     draw_top("Top 10 " + selection + " by Number of Certified Cases", column, certified_cases, "Number of Certified Applications", "orange")
     draw_top("Top 10 " + selection + " by Number of Denied Cases", column, denied_cases, "Number of Denied Applications", '#1f77b4')
     draw_rate("Bottom 10 " + selection + " by Cerfication Rate", column)
-    write_result(column)
+    write_result(column, selection)
 else:
     column = columns_2[features_2.index(selection)]
     draw_distribution("Percentage of Certified Applications by " + selection, column)
